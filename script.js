c4d04ed770db9209e865ed97fbf67a15844efd38
Venkat Polumahanti <venkat4541@gmail.com>
2017-03-16 02:25:35 -0400

    Functioning well with select filtering

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ var app = angular.module('guvApi', []);
 
 app.controller('listApi', ['$scope', '$http', function($scope, $http) {
   $http.get('https://pages.18f.gov/API-All-the-X/data/individual_apis.json').then(function(data) {
-    var info = data.data;
-    $scope.name = info[0].name;
-    $scope.org = info[0].apis[0].name;
-    $scope.url = info[0].apis[0].url;
-    console.log($scope.name);
-    console.log($scope.org);
-    console.log($scope.url);
+    $scope.info = data.data;
   });
 }]);
